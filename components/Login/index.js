0b9a9b6461fdce82d0dbd8d2d9a4cc9bf853ae3a
Sriram <sriram.pant5@gmail.com>
2023-03-11 21:18:59 +0530

    minor changes

--- components/Login/index.js
+++ components/Login/index.js
@@ -31,9 +31,9 @@ const Login = () => {
         />
       </div>
       {/* <Image src={background} alt="background" /> */}
-      <div className="text-white rounded-2xl shadow-lg shadow-grey-500 h-[70%] w-[50%] flex flex-col items-center justify-between">
+      <div className="text-white rounded-2xl shadow-lg shadow-grey-500 h-[70%] w-[50%] flex flex-col items-center justify-between border-solid border-2 border-orange-300 lg:w-[30%]">
         <div className="w-[70%] flex flex-col items-center">
-          <span className=" text-4xl font-semibold mt-5">Login</span>
+          <span className="text-4xl font-semibold mt-5">Login</span>
           <div className="w-full relative my-7 border-solid border-b-[2px] border-b-orange-300 ">
             <input
               className="peer/input w-full outline-none p-3 pl-2 text-m bg-transparent"
@@ -57,13 +57,13 @@ const Login = () => {
             </div>
           </div>
           <div className="w-full text-xs cursor-pointer flex justify-end">Forget Password ?</div>
-          <div className="w-full my-5 border-solid border-[1px] border-orange-400 rounded-full flex items-center justify-center p-1 text-orange-400 hover:text-white hover:bg-orange-400 cursor-pointer ease-in duration-300">
+          <div className="w-full my-5 py-2 border-solid border-[1px] border-orange-400 rounded-full flex items-center justify-center p-1 text-orange-400 hover:text-white hover:bg-orange-400 cursor-pointer ease-in duration-300">
             Login
           </div>
           <div>Or</div>
-          <div className="mt-2 flex items-center w-full bg-[#4285f4] text-white font-medium rounded-md">
+          <div className="mt-2 flex items-center  bg-[#4285f4] text-white font-medium rounded-md">
             <Image src={GoogleIcon} width="50" alt="Google" />
-            <span className="pl-2">Sign In with Google</span>
+            <span className="px-2">Sign In with Google</span>
           </div>
         </div>
         <div className="mb-10">
